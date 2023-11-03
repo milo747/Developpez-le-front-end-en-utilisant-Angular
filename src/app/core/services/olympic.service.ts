13a8b2648a29b60fc8c0703639e0a4541e6a4b96
milo747 <awmilo747@gmail.com>
2023-11-04 00:15:40 +0100

    Bloqué sur l'ajout des variables de tableau country dans le modele

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -58,4 +58,8 @@ export class OlympicService {
   getOlympics() {
     return this.olympics$.asObservable();
   }
+
+  getAllCountry(): OlympicCountry[] {
+    return this.country;
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { OlympicCountry } from 'src/app/core/models/Olympic.model';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -9,12 +10,17 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<string | null> = of(null);
-
-  constructor(private olympicService: OlympicService) {}
+  public country: OlympicCountry[];
+  public test: string = 'toto';
+  constructor(private olympicService: OlympicService) {
+    this.country = [];
+  }
 
   ngOnInit(): void {
+    console.log(this.country);
+    this.country = this.olympicService.getAllCountry();
+    console.log(this.country);
     this.olympics$ = this.olympicService.getOlympics();
-
     //console.log(this.olympics$.subscribe((value) => console.log(value)));
     /*this.olympics$.subscribe((value) => {
       console.log(value);
