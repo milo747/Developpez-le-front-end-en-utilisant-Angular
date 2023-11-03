cc41a00e96e5f81234e963fec48d8cb7cfd1be45
milo747 <awmilo747@gmail.com>
2023-11-04 00:53:28 +0100

    essai template sur home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,10 +9,16 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
-
+  public var: string = 'kkk';
+  public var2: string[] = ['kkk', 'ttt'];
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    console.log(
+      this.olympics$.subscribe((value) => {
+        this.var = 'tata';
+      })
+    );
   }
 }
